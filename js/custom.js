1bee3647d7076279a362618ae5a44c3819674498
Zitzabis <zitzabis.zitz@gmail.com>
2018-03-27 17:01:54 -0400

    JS: Submits proper data and spits out results.

--- js/custom.js
+++ js/custom.js
@@ -7,50 +7,43 @@ function addColor() {
     colorArray = color.split(',');
     for (i = 0; i < colorArray.length; ++i) {
         color = colorArray[i].replace(/\s/g, '');; // Pulls hex and strips spaces
-
         if (color.charAt(0) != '#') {
+            hexs.push(color);
             color = "#" + color;
+        }
+        else {
+            hexs.push(color.substring(1));
         }
 
         code = '<span class="badge" style="background-color: ' + color + ';">' + color + '</span> ';
         content = document.getElementById('colors').innerHTML;
         document.getElementById('colors').innerHTML = content + code;
-
-        hexs.push(color);
     }
     document.getElementById('hexcode').value = "";
 }
 
 // Runs matching
 function matcher() {
-    // Build hexs array
-    jsonArray = '[ ';
-    for (i = 0; i < hexs.length; ++i) {
-        jsonArray += '"' + hexs[i] + '"';
-        if (i != hexs.length - 1) {
-            jsonArray += ', ';
-        }
-    }
-    jsonArray += ' ]';
-    console.log(jsonArray);
-
-    // Extract URL
-    urlString = document.getElementById('url').value;
-    console.log(urlString);
+    object = JSON.stringify({
+        Hexs: hexs,
+        URL: document.getElementById('url').value
+    })
+    console.log("Send data: " + object);
 
     // POST the data
     $.ajax({
         type: "POST",
         url: 'http://api.zitzasoft.com/api/palette_matcher/match',
-        data: JSON.stringify({
-            Hexs:jsonArray,
-            URL:urlString
-        }),
+        data: object,
         dataType: 'json',
         contentType: "application/json",
         success: function(data)
         {
             console.log("Response: " + data["match"]);
+            if (data["match"])
+                document.getElementById('status').innerHTML = '<span class="badge bg-success">The skin matches the palette!</span>';
+            else
+                document.getElementById('status').innerHTML = '<span class="badge bg-danger">The skin does not match the palette.</span>';
         } 
     });
 }
