311190097ffd3b5c49c40c43522039717dc9fc1b
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-07-30 00:47:10 -0400

    Adds hexcode validation.

--- js/custom.js
+++ js/custom.js
@@ -6,21 +6,45 @@ function addColor() {
     var color = document.getElementById('hexcode').value;
     colorArray = color.split(',');
     for (i = 0; i < colorArray.length; ++i) {
-        color = colorArray[i].replace(/\s/g, '');; // Pulls hex and strips spaces
-        if (color.charAt(0) != '#') {
-            hexs.push(color);
-            color = "#" + color;
-        }
-        else {
-            hexs.push(color.substring(1));
-        }
+        color = colorArray[i].replace(/\s/g, ''); // Pulls hex and strips spaces
+        // Only logs color if valid hex value
+        if (isValidHex(color)) {
+            if (color.charAt(0) != '#') {
+                hexs.push(color);
+                color = "#" + color;
+            }
+            else {
+                hexs.push(color.substring(1));
+            }
 
-        code = '<span class="badge whiteOutline" style="background-color: ' + color + ';">' + color + '</span> ';
-        content = document.getElementById('colors').innerHTML;
-        document.getElementById('colors').innerHTML = content + code;
+            code = '<span class="badge whiteOutline" style="background-color: ' + color + ';">' + color + '</span> ';
+            content = document.getElementById('colors').innerHTML;
+            document.getElementById('colors').innerHTML = content + code;
+        }
     }
     document.getElementById('hexcode').value = "";
 }
+
+ /**
+   * Validates hex value
+   * @param  {String} color hex color value
+   * @return {Boolean}
+   */
+  function isValidHex(color) {
+    if(!color || typeof color !== 'string') return false;
+
+    // Validate hex values
+    if(color.substring(0, 1) === '#') color = color.substring(1);
+
+    switch(color.length) {
+      case 3: return /^[0-9A-F]{3}$/i.test(color);
+      case 6: return /^[0-9A-F]{6}$/i.test(color);
+      case 8: return /^[0-9A-F]{8}$/i.test(color);
+      default: return false;
+    }
+
+    return false;
+  }
 
 // Runs matching
 function matcher() {
