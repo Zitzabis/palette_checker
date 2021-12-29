b953db760453db1fe85988fdd4cc243ac800ed06
Zitzabis <zitzabis.zitz@gmail.com>
2021-12-29 16:36:45 -0500

    Bootstrap update.

--- components/head.php
+++ components/head.php
@@ -1,7 +1,7 @@
 <head>
-    <title>Palette Checker</title>
+    <title>MC Voter Tool</title>
     <link rel="stylesheet" href="css/custom.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/sidebar.css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet">
 </head>
